505010f0edd0d4868836eb1e2dd24234b2d13c14
Tony Albano <tony.d.albano@gmail.com>
2016-04-18 11:49:01 -0500

    Add to.data.frame to rsim

--- R/rsim.R
+++ R/rsim.R
@@ -12,7 +12,10 @@
 #' correlate at \code{rho} with \code{x}.
 #' @param meanx,sdx the desired mean and standard deviation of x.
 #' @param meany,sdy the desired mean and standard deviation of y.
-#' @return Returns a list containing:
+#' @param to.data.frame should the results be converted to a \code{data.frame}?
+#' Defaults to \code{FALSE}.
+#' @return When \code{to.data.frame} is \code{TRUE}, returns a data frame of
+#' scores on x and y. Otherwise, a list containing:
 #' \describe{
 #' \item{x}{\code{x} when supplied, or the simulated vector of scores}
 #' \item{y}{the vector y sampled from a population correlating with
@@ -22,20 +25,25 @@
 #' \item{meanx, sdx, meany, sdy}{the means and standard deviations of x and y}
 #' }
 #' @export
-rsim <- function(n, rho, x, meanx = 0, sdx = 1, meany = 0, sdy = 1) {
+rsim <- function(n, rho, x, meanx = 0, sdx = 1, meany = 0, sdy = 1,
+  to.data.frame = FALSE) {
+  if(missing(x))
+    x <- rnorm(n, meanx, sdx)
+  else {
+    n <- length(x)
+    meanx <- mean(x)
+    sdx <- sd(x)
+  }
 
-    if(missing(x))
-        x <- rnorm(n, meanx, sdx)
-    else {
-        n <- length(x)
-        meanx <- mean(x)
-        sdx <- sd(x)
-    }
+  y <- (x - meanx) * rho +
+  	rnorm(n, 0, sdx) * sqrt(1 - rho^2)
+  y <- y * sdy / sdx + meany
 
-    y <- (x - meanx) * rho +
-    		rnorm(n, 0, sdx) * sqrt(1 - rho^2)
-    y <- y * sdy / sdx + meany
+  if(to.data.frame)
+    out <- data.frame(x = x, y = y)
+  else
+    out <- list(x = x, y = y, rho = rho, r = cor(x, y),
+      meanx = meanx, sdx = sdx, meany = meany, sdy = sdy)
 
-    return(list(x = x, y = y, rho = rho, r = cor(x, y),
-      meanx = meanx, sdx = sdx, meany = meany, sdy = sdy))
+  return(out)
 }
