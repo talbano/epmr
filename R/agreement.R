2b34b33797a05ead8b5ef47e97860e543cd58bb9
Tony Albano <tony.d.albano@gmail.com>
2016-04-18 16:43:17 -0500

    Fix factoring in agreement index

--- R/agreement.R
+++ R/agreement.R
@@ -4,25 +4,47 @@
 #'
 #' @param x,y vectors of scores from two different raters for the same units
 #' of observation. Alternatively, if \code{y} is missing, \code{x} can be a
-#' matrix of scores with two columns, one per rater.
+#' matrix of scores with two columns, one per rater, or a crosstab of rater
+#' scores.
+#' @param w matrix of weights applied to adjust the severity of disagreements
+#' in calculating weighted kappa. Be default, cells 1 off from the diagonal
+#' are weighted 1, 2 off are weighted 2, etc.
 #' @param lev vector of all unique possible scores.
 #' @return Returns a list of agreement indices.
 #' @export
-agranaly <- function(x, y, lev = unique(c(x))) {
+agranaly <- function(x, y, w, lev) {
 
-  if(!missing(y))
+  if (!missing(y))
     x <- as.matrix(data.frame(x, y))
-  if(ncol(x) > 2)
-    stop("too many variables provided in 'x'")
+  if (dim(x)[1] != dim(x)[2]) {
+    if (ncol(x) > 2)
+      stop("too many variables provided in 'x'")
+    if (missing(lev))
+      lev <- sort(unique(c(unlist(x))))
+    x[, 1] <- factor(x[, 1], levels = lev)
+    x[, 2] <- factor(x[, 2], levels = lev)
+    xtab <- table(x[, 1], x[, 2])
+  }
+  else xtab <- x
+  if(any(is.na(xtab)))
+      stop("crosstab contains NAs")
 
-  x[, 1] <- factor(x[, 1], levels = lev)
-	x[, 2] <- factor(x[, 2], levels = lev)
-	ptab <- table(x[, 1], x[, 2])
+  n <- sum(xtab)
+  ftab <- table(x[, 1], x[, 2])
+  rt <- rowSums(ftab)
+  ct <- colSums(ftab)
+  ptab <- (rt %*% t(ct)) / n
+	po <- sum(diag(ftab))
+	pc <- sum(diag(ptab))
+  if(missing(w)) {
+    ns <- nrow(xtab) - 1
+    w <- matrix(sapply(0:ns, function(i) abs(0:ns - i)),
+      nrow = ns + 1)
+  }
 
-  n <- sum(ptab)
-	po <- sum(diag(ptab))/n
-	pc <- sum((rowSums(ptab)/n) * (colSums(ptab)/n))
-	out <- list(pctagree = po, kappa = (po - pc)/(1 - pc))
+  out <- c(agree = po / n,
+    kappa = (po - pc)/(n - pc),
+    wkappa = 1 - sum(ftab * w)/sum(ptab * w))
 
 	return(out)
 }
