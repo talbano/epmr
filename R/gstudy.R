a5d2cb5a2f31f62b578f77441180e8e558a7cfd3
Tony Albano <tony.d.albano@gmail.com>
2016-04-19 20:15:36 -0500

    Fixing S3 methods for gstudy

--- R/gstudy.R
+++ R/gstudy.R
@@ -13,26 +13,29 @@
 #' @param id character string naming the id variable for the unit of observation,
 #' defaulting to \code{"person"}.
 #' @param ... further arguments passed to or from other methods.
-#' @return Returns a list containing the g and d coefficients, the variance
-#' components, and n counts by facet.
+#' @return Returns a list containing reliabilities, variance components, and
+#' n counts by facet.
 #' @export
 gstudy <- function(x, ...) UseMethod("gstudy")
 
 #' @describeIn gstudy Method defaults to formula interface.
+#' @export
 gstudy.default <- function(x, data = NULL, ...) {
 
   gstudy.formula(x, data, ...)
 }
 
 #' @describeIn gstudy Method for formula objects.
+#' @export
 gstudy.formula <- function(x, data = NULL, ...) {
 
   mer <- lme4::lmer(x, data = data, ...)
-  return(gstudy.merMod(mer))
+  gstudy.merMod(mer)
 }
 
 #' @describeIn gstudy Method for wide data frames with people as rows and a
 #' single facet as columns
+#' @export
 gstudy.data.frame <- function(x, ...) {
 
   x <- x[complete.cases(x), ]
@@ -42,13 +45,13 @@ gstudy.data.frame <- function(x, ...) {
     rep(1:nc, each = nr))
   colnames(xl)[3] <- "rater" # Add support for different name
 
-  return(gstudy.formula(score ~ 1 + (1 | person) + (1 | rater),
-    data = xl, ...))
-
+  gstudy.formula(score ~ 1 + (1 | person) + (1 | rater),
+    data = xl, ...)
 }
 
 #' @describeIn gstudy Method for objects of class \code{\link{merMod}}.
-gstudy.merMod <- function(x, n, id = "person") {
+#' @export
+gstudy.merMod <- function(x, n, id = "person", ...) {
 
   vc <- unlist(lme4::VarCorr(x))
   s2 <- attr(lme4::VarCorr(x), "sc")^2
@@ -68,15 +71,19 @@ gstudy.merMod <- function(x, n, id = "person") {
   #  ns <- ns[fnames]
   #}
 
-  g <- vc[id] / sum(vc[id], (vc / ns)[fnames != id &
-    grepl(id, names(vc))], s2 / prod(n[nnames != id]))
-  d <- vc[id] / sum(vc[id], (vc / ns)[fnames != id],
+  ga <- vc[id] / sum(vc[id], vc[fnames != id],
     s2 / prod(n[nnames != id]))
-  names(g) <- names(d) <- NULL
-
+  gr <- vc[id] / sum(vc[id], vc[fnames != id &
+    grepl(id, names(vc))], s2 / prod(n[nnames != id]))
+  da <- vc[id] / sum(vc[id], (vc / ns)[fnames != id],
+    s2 / prod(n[nnames != id]))
+  dr <- vc[id] / sum(vc[id], (vc / ns)[fnames != id &
+    grepl(id, names(vc))], s2 / prod(n[nnames != id]))
+  r <- data.frame(absolute = c(ga, da), relative = c(gr, dr),
+    row.names = c("g", "d"))
 
   vcout <- data.frame("variance" = c(vc, s2))
   rownames(vcout)[nrow(vcout)] <- "residual"
 
-  return(list(g = g, d = d, vc = vcout, ns = ns))
+  return(list(r = r, vc = vcout, ns = ns))
 }
