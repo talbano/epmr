ae50bc3ef07c906accd1235a59750df27ed9291b
Tony Albano <tony.d.albano@gmail.com>
2016-04-27 17:48:24 -0500

    Starting mstudy for running multiple analyses

---
+++ R/mstudy.R
@@ -0,0 +1,20 @@
+#' Measurement Scale Study
+#'
+#' Functions for running multiple studies on a set of scored item responses,
+#' including a descriptive study of the total scale scores, and item,
+#' internal consistency reliability, and item response theory studies.
+#'
+#' @param x matrix or data.frame of scored item responses.
+#' @param subset optional vector for selecting a subset of columns from \code{x}.
+#' @param scores optional vector of construct scores.
+#' @return Returns a list with elements pertaining to each study, including:
+#' \code{descriptives}, \code{items}, \code{reliability}, and \code{irt}.
+#' @export
+mstudy <- function(x, subset = 1:ncol(x), scores, complete = TRUE,
+  na.rm = FALSE) {
+
+  itm <- istudy(x, subset = subset, scores = scores)
+  des <- dstudy(rowSums(x, na.rm = TRUE), complete = complete,
+    na.rm = na.rm)
+  rel <- rstudy(x)
+}
