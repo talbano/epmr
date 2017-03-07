090a960c52f3f4871bfeeb53c654345af4207410
Tony Albano <tony.d.albano@gmail.com>
2017-03-07 16:36:56 -0600

    Updating scree plot and documentation

--- R/fastudy.R
+++ R/fastudy.R
@@ -1,32 +1,46 @@
 #' Factor Analysis Study
 #'
 #' Functions for examining the dimensionality of a data set via
-#' exploratory factor analysis.
+#' exploratory factor analysis. Currently a simple wrapper for factanal.
 #'
 #' @param x matrix or data.frame of scored item responses, one row per person,
 #' one column per item.
+#' @param factors the number of factors to extract.
+#' @param covmat optional covariance matrix. Not used if \code{x} is supplied.
 #' @param complete logical with default \code{FALSE} indicating whether or not
 #' \code{x} should be reduced to rows with complete data across all columns.
 #' @param ... further arguments passed to or from other functions.
+#' @param ylim vector of limits for y-axis in the scree plot, passed to \code{par}.
+#' @param h y-axis value specifying a horizonal line the scree plot, passed to
+#' \code{abline}.
+#' @return \code{fastudy} runs an exploratory factor analysis using the
+#' \code{factanal} function, and returns a matrix of factor loadings. The
+#' print method displays eigenvalues and proportion of variance explained
+#' per factor.
+#'
 #' @export
-fastudy <- function(x, factors, complete = TRUE, ...) {
-  if (complete) x <- x[complete.cases(x), ]
-  out <- factanal(x, factors = factors, ...)
+fastudy <- function(x, factors, covmat, complete = TRUE, ...) {
+  if (!missing(x)) {
+    if (complete) x <- x[complete.cases(x), ]
+    out <- factanal(x, factors = factors, ...)
+  } else if (!missing(covmat)) {
+    out <- factanal(covmat = covmat, factors = factors, ...)
+  } else stop("either 'x' or 'covmat' must be supplied")
   class(out) <- c("fastudy", "factanal")
   return(out)
 }
 
 #' @rdname fastudy
 #' @export
-plot.fastudy <- function(x, ylim = c(1, nrow(x$loadings)), ...) {
+plot.fastudy <- function(x, ylim = c(1, nrow(x$loadings)), h, ...) {
   nf <- ncol(x$loadings)
-  #ni <- nrow(x$loadings)
   ei <- apply(x$loadings, 2, function(y) sum(y^2))
   plot(c(1, nf), ylim, type = "n", xaxt = "n", xlab = "Factor",
     ylab = "Eigenvalue", xlim = c(1 - nf * .1, nf + nf * .1))
   axis(1, at = 1:nf, labels = 1:nf)
   points(1:nf, ei)
   lines(1:nf, ei)
+  if (!missing(h)) abline(h = h)
 }
 
 #' @export
