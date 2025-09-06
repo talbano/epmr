3dec6a2d259ca3a5243a3c4204a77b57b114241f
“talbano” <tony.d.albano@gmail.com>
2025-09-06 10:55:07 -0700

    Updating package description and help

---
+++ R/epmr-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' @importFrom graphics abline axis lines plot points
+#' @importFrom stats AIC BIC complete.cases cor deviance df.residual
+#' factanal formula logLik median na.omit quantile rnorm sd var pchisq
+#' na.omit runif glm relevel "contrasts<-" mantelhaen.test
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
